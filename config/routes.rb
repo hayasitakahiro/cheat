38b2f480062a086b179e0aa6fe65f6d85b982d1a
hayasitakahiro <0kv1td3.7254p9e@au.com>
2022-08-03 21:57:23 +0900

    ルーティング、コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'cheats/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "cheats#index"
+
+
+
 end
